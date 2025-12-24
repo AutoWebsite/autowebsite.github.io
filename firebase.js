8ba41246a90a6ad0aae0f3b14925487178885f35
AutoWebsite <contactvibetrolls@gmail.com>
2025-12-25 00:48:53 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,24 @@
 <script type="module">
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+  // Firebase 12.7.0 imports
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+  import { getAuth, GoogleAuthProvider, signInWithPopup, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
+  import { getFirestore, collection, addDoc, getDocs, Timestamp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBIoX7KC131wFiGsmwIc-hWp6td6Yu7qWY",
-  authDomain: "autowebsite-testing.firebaseapp.com",
-  projectId: "autowebsite-testing",
-};
+  // Your Firebase config
+  const firebaseConfig = {
+    apiKey: "AIzaSyBIoX7KC131wFiGsmwIc-hWp6td6Yu7qWY",
+    authDomain: "autowebsite-testing.firebaseapp.com",
+    projectId: "autowebsite-testing",
+    storageBucket: "autowebsite-testing.appspot.com",
+    messagingSenderId: "86153129960",
+    appId: "1:86153129960:web:6a60b1bcbddfb12c6f7e5d"
+  };
 
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  const db = getFirestore(app);
+
+  // Export for other modules (if using multiple files)
+  export { auth, db, GoogleAuthProvider, signInWithPopup, createUserWithEmailAndPassword, signInWithEmailAndPassword, collection, addDoc, getDocs, Timestamp };
 </script>
